bdec965f7aff34f4ffe5a6f8ec16d62a3b206dd6
Evgenii Zelenin <transingularity@gmail.com>
2019-04-04 13:12:56 +0300

    Added simple game of guess the number

---
+++ guess-the-number/guessNumber.py
@@ -0,0 +1,20 @@
+
+import random, sys
+
+def checkNum(num):
+    global cNum
+    if num < cNum:
+        print('Your number is too low')
+    elif num > cNum:
+        print('Your number is too high')
+    elif num == cNum:
+        print('You guessed right')
+        sys.exit()
+        
+print('I am thinking of a number between 1 and 10.\n')
+cNum = random.randint(1, 10)
+
+while True:
+    print('Take a guess:')
+    myNum=input()
+    checkNum(int(myNum))
