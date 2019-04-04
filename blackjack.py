14d6e0d7939ff776daba0e133910bb841784f901
Evgenii Zelenin <transingularity@gmail.com>
2019-04-04 11:59:57 +0300

    Revorked code using methods

--- blackjack.py
+++ blackjack.py
@@ -2,8 +2,36 @@
  
 import random, sys
 
+# Method to increase dealer's hand
+def incDhand(dtotal):
+    if dtotal <= 20:
+        dt = random.randint(1, 11)
+        dtotal = dtotal + dt
+        return dtotal
+    else:
+        return dtotal
+
+# Method to increase player's hand
+def incPhand(ptotal):
+    pt = random.randint(1, 11)
+    ptotal=ptotal+pt
+    return ptotal
+
+# Method to decide winner
+def whois(ptotal, dtotal):
+    if ptotal <= 21 and ptotal > dtotal:
+        return 'You win'
+    elif ptotal > 21:
+        return 'You lose'
+    elif ptotal <= 21 and dtotal > 21:
+        return 'You win'
+    elif dtotal == 21 and ptotal < 21:
+        return 'You lose'
+
+
 ptotal=0
 dtotal=0
+
 while True:
     print('Player\'s turn.\n')
     pt = random.randint(1, 11)
@@ -21,41 +49,38 @@ while True:
         panswer = input()
         if panswer == 'yes':
             while panswer == 'yes':
-                pt = random.randint(1, 11)
-                ptotal=ptotal+pt
-                print('Your hand: ' + str(ptotal))
-            
-                if dtotal <= 20:
-                    dt = random.randint(1, 11)
-                    dtotal = dtotal + dt
-                    print('Dealer\'s hand: ' + str(dtotal))
-                else:
-                    print('Dealer\'s hand: ' + str(dtotal))
-                    
+                ptotal=incPhand(ptotal)
+                dtotal=incDhand(dtotal)
+                print('Your\'s hand ' + str(ptotal)) 
+                print('Dealer\'s hand ' + str(dtotal))
+                
                 print('Do you need more? Type yes or no')
                 panswer = input()
                 if panswer == 'yes':
                     continue
                 elif panswer == 'no':
-                    while dtotal <= 20:
-                        print('Your hand: ' + str(ptotal))
-                        dt = random.randint(1, 11)
-                        dtotal = dtotal + dt
-                        print('Dealer\'s hand: ' + str(dtotal))
-                    
-                    print('\nTotal hands:\nYour\'s ' + str(ptotal) + '\nDealer\'s ' + str(dtotal))
-                    if ptotal <= 21 and ptotal > dtotal:
-                        print('You win')
-                        sys.exit()
-                    elif ptotal > 21:
-                        print('You lose')
-                        sys.exit()
-                    elif ptotal <= 21 and dtotal > 21:
-                        print('You win')
-                        sys.exit()
-                    elif dtotal == 21 and ptotal < 21:
-                        print('You lose')
-                        sys.exit()                
+                    dtotal=incDhand(dtotal)
+                    print('Dealer\'s hand ' + str(dtotal))
+            
+                print('\nTotal hands:\nYour\'s ' + str(ptotal) + '\nDealer\'s ' + str(dtotal))
+            
+        elif panswer == 'no':
+            dtotal=incDhand(dtotal)
+            print('\nTotal hands:\nYour\'s ' + str(ptotal) + '\nDealer\'s ' + str(dtotal))
+            if dtotal <= 20:
+                continue
+            else:
+                winner=whois(ptotal, dtotal)
+                print(winner)
+                sys.exit()
+            
+winner=whois(ptotal, dtotal)
+print(winner)
+sys.exit()
+        
+            
+            
+            
             
             
             
